ac8892b1a52731b5f6d692619d62ca67e10eeb37
MY0usuf <muhammady.be22@uceou.edu>
2024-11-22 09:51:41 +0000

    Fixed issue with databse
    Databse wasnt inputting property properly to investor, fixed it

--- models/models.py
+++ models/models.py
@@ -4,23 +4,26 @@ from sqlalchemy import Date
 from flask_login import UserMixin
 
 class User(UserMixin,db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
 class Investor(db.Model):
+    __tablename__ = 'investor'
     id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     '''    contact_number = db.Column(db.String(20), nullable=False)
     passport_number = db.Column(db.String(20), nullable=False)
     emirates_id_number = db.Column(db.String(20), nullable=False)'''
-    properties = db.relationship('Property', backref='owner', lazy=True, cascade='all, delete-orphan')
+    properties = db.relationship('Property', backref='investor', lazy=True, cascade='all, delete-orphan')
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('investors', lazy=True))
+    user = db.relationship('User', backref=db.backref('investor', lazy=True))
     document = db.Column(db.String(120), nullable=True)
 
 class Tenant(db.Model):
+    __tablename__ = 'tenant'
     id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     contact_number = db.Column(db.String(20), nullable=False)
@@ -37,6 +40,7 @@ class Tenant(db.Model):
     document = db.Column(db.String(120), nullable=True)
 
 class Property(db.Model):
+    __tablename__ = 'property'
     id = db.Column(db.String, primary_key=True)
     building_name = db.Column(db.String(100), nullable=False)
     unit_no = db.Column(db.String(100), nullable=False)
